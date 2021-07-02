33a43bb148504f9001f3c21dc2f733edc1306aff
Annika Bassey <70558054+abassey2@users.noreply.github.com>
2021-07-02 02:06:02 +0000

    Updated code with useContext

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,45 @@
 import { useState } from "react";
 import LoadClients from "./LoadClients"
 import LoadClientDetails from "./LoadClientDetails";
+import LoadClientContext from "./LoadClientContext";
+
+const loadclients = [
+  {
+    name: "Load-Client-ID-001",
+    version: 5.0,
+    id: 1,
+    region: "california",
+    zone: "colorado",
+    scheduler: "nev",
+    currstatus: "ready",
+    metrics: "prometheus",
+    isExecute: false,
+  },
+  {
+    name: "Load-Client-ID-002",
+    version: 5.0,
+    id: 2,
+    region: "california",
+    zone: "colorado",
+    scheduler: "nev",
+    currstatus: "unresponsive",
+    metrics: "prometheus",
+    isExecute: false,
+  },
+  {
+    name: "Load-Client-ID-003",
+    version: 5.0,
+    id: 3,
+    region: "california",
+    zone: "colorado",
+    scheduler: "nev",
+    currstatus: "busy",
+    metrics: "prometheus",
+    isExecute: false,
+  }
+];
 
 function App() {
-
-  const loadclients = [
-    {
-      name: "Load-Client-ID-001",
-      version: 5.0,
-      id: 1,
-      region: "california",
-      zone: "colorado",
-      scheduler: "nev",
-      currstatus: "ready",
-      metrics: "prometheus",
-      isExecute: false,
-    },
-    {
-      name: "Load-Client-ID-002",
-      version: 5.0,
-      id: 2,
-      region: "california",
-      zone: "colorado",
-      scheduler: "nev",
-      currstatus: "unresponsive",
-      metrics: "prometheus",
-      isExecute: false,
-    },
-    {
-      name: "Load-Client-ID-003",
-      version: 5.0,
-      id: 3,
-      region: "california",
-      zone: "colorado",
-      scheduler: "nev",
-      currstatus: "busy",
-      metrics: "prometheus",
-      isExecute: false,
-    }
-  ];
 
   const [isOpen, setIsOpen] = useState(false)
   const [ currClientDetails, setCurrClientDetails ] = useState(-1)
@@ -59,12 +60,11 @@ function App() {
 
   return (
     <div className="App">
-      <LoadClients openPopup={handleOpen} clientDetails={loadclients}/> 
-      {isOpen && <LoadClientDetails
-          clientDetails={loadclients}
-          clientIndex={currClientDetails}
-          closePopup={handleClose}
-        />}
+      <LoadClientContext.Provider 
+        value={{ loadclients, currClientDetails, setCurrClientDetails, handleOpen, handleClose }}>
+        <LoadClients/> 
+        {isOpen && <LoadClientDetails/>}
+      </LoadClientContext.Provider>
     </div>
   );
 }
--- client/src/LoadClientDetails/index.js
+++ client/src/LoadClientDetails/index.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import LoadClientContext from "../LoadClientContext";
 
 import "./styles.css"
  
 const LoadClientDetails = props => {
-  console.log(props.clientDetails[props.clientIndex])
+  const { loadclients, currClientDetails, handleClose } = useContext(LoadClientContext);
+
   return (
     <div className="main">
       <div className="popup-box">
         <div className="box">
-          <span className="close-icon" onClick={props.closePopup}>x</span>
-          <h1>{props.clientDetails[props.clientIndex].name}</h1>
-          <p>version: {props.clientDetails[props.clientIndex].version}</p>
-          <p>ID: {props.clientDetails[props.clientIndex].id}</p>
-          <p>Region: {props.clientDetails[props.clientIndex].region}</p>
-          <p>Zone: {props.clientDetails[props.clientIndex].zone}</p>
-          <p>Scheduler: {props.clientDetails[props.clientIndex].scheduler}</p>
-          <p>Status: {props.clientDetails[props.clientIndex].currstatus}</p>
-          <p>Metrics: {props.clientDetails[props.clientIndex].metrics}</p>
+          <span className="close-icon" onClick={handleClose}>x</span>
+          <h1>{loadclients[currClientDetails].name}</h1>
+          <p>version: {loadclients[currClientDetails].version}</p>
+          <p>ID: {loadclients[currClientDetails].id}</p>
+          <p>Region: {loadclients[currClientDetails].region}</p>
+          <p>Zone: {loadclients[currClientDetails].zone}</p>
+          <p>Scheduler: {loadclients[currClientDetails].scheduler}</p>
+          <p>Status: {loadclients[currClientDetails].currstatus}</p>
+          <p>Metrics: {loadclients[currClientDetails].metrics}</p>
         </div>
       </div>
     </div>
