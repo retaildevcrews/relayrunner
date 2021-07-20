117fb4621f8c0b23c9ac196ec78a489a378f7921
Annika Bassey <70558054+abassey2@users.noreply.github.com>
2021-07-20 18:40:22 +0000

    Updated UI for viewing load test overview

--- client/src/components/Configs/index.js
+++ client/src/components/Configs/index.js
@@ -1,51 +1,30 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
 import { ConfigsContext } from '../../contexts';
 
 import "./styles.css"
 
 const Configs = () => {
-    const { configs, loadtests } = useContext(ConfigsContext);
+    const { configs, loadTests } = useContext(ConfigsContext);
 
-    const [currConfig, setCurrConfig] = useState(-1);
-    const [currLoadTests, setCurrLoadTests] = useState([]);
-    const [selectedLoadTest, setSelectedLoadTest] = useState(-1);
-
-    console.log(currLoadTests)
-    console.log(currConfig)
-    
-    useEffect(() => {
-        setCurrLoadTests([]);
-        if (currConfig > 0) {
-            for (let i in loadtests) {
-                if (loadtests[i].config === currConfig) {
-                    setCurrLoadTests(loadtest => {
-                        return [
-                            ...loadtest, 
-                            loadtests[i]
-                        ]
-                    });
-                    continue;
-                }
-            };
-        }
-    }, [currConfig, loadtests])
+    const [currConfigId, setcurrConfigId] = useState(-1);
+    const [selectedLoadTestId, setselectedLoadTestId] = useState(-1);
         
     const configSelect = (id) => () => {
-        currConfig === id ? setCurrConfig(-1) : setCurrConfig(id);
+        currConfigId === id ? setcurrConfigId(-1) : setcurrConfigId(id);
     }
 
     const loadTestSelect = (id) => {
-        selectedLoadTest === id ? setSelectedLoadTest(-1) : setSelectedLoadTest(id);
+        selectedLoadTestId === id ? setselectedLoadTestId(-1) : setselectedLoadTestId(id);
     }
 
-    const config = configs.find((c) => c.id === currConfig);
-    const loadTestPath = loadtests.find((c) => c.id === selectedLoadTest);
+    const config = configs.find((c) => c.id === currConfigId);
+    const loadTestPath = loadTests.find((c) => c.id === selectedLoadTestId);
 
     return (
         <div className="main">
             <div id="configpath">
                 {config && <p><b>{config.name} &nbsp;</b></p>}
-                {config && loadTestPath && config.id === loadTestPath.config && <p><b> > {loadTestPath.name} </b></p>}
+                {config && loadTestPath && config.id === loadTestPath.configId && <p><b>{'> '}{loadTestPath.name} </b></p>}
             </div>
             <hr className="horizontal1"></hr>
             <hr className="horizontal2"></hr>
@@ -56,7 +35,7 @@ const Configs = () => {
                     {
                         configs.map((c) => (
                             <li key={c.id}>
-                                <button className={`configslist ${c.id === currConfig ? "selected" : ""}`} onClick={configSelect(c.id)}>{c.name}</button>
+                                <button className={`configslist ${c.id === currConfigId ? "selected" : ""}`} onClick={configSelect(c.id)}>{c.name}</button>
                             </li>
                         ))
                     }
@@ -65,17 +44,16 @@ const Configs = () => {
             </div>
             <hr className="vertical"></hr>
             <div className="loadtests">
-                {currConfig > 0 && 
-                <h1>LoadTests</h1>}
+                {currConfigId > 0 && 
+                <h1>Load Tests</h1>}
                 <div>
                     <ul>
                     {
-                        currLoadTests.map((lt) => (
+                        loadTests.filter(lt => lt.configId === currConfigId).map((lt) => (
                             <li key={lt.id}>
-                                <button className={`configslist ${lt.id === selectedLoadTest ? "selected" : ""}`} onClick={() => {loadTestSelect(lt.id)}}>{lt.name}</button>
+                                <button className={`configslist ${lt.id === selectedLoadTestId ? "selected" : ""}`} onClick={() => {loadTestSelect(lt.id)}}>{lt.name}</button>
                             </li>
-                        )
-                        )
+                        ))
                     }
                     </ul>
                 </div>
